694f932f8841a18c3315e6d5cd02060252e654cd
vigneshsaravanakumar404 <10021913@sbstudents.ir>
2023-06-11 19:04:51 -0400

    Everything but exception catching

--- Main.py
+++ Main.py
@@ -4,36 +4,144 @@ import json
 import time
 import pyuac
 
-# Methods
-
+# Variables
 app = Flask(__name__)
+dict = []
+stockfish = Stockfish(
+    path=r"C:\\Users\\Vigne\\Downloads\\stockfish_15.1_win_x64_popcnt\\stockfish_15.1_win_x64_popcnt\\stockfish-windows-2022-x86-64-modern.exe")
 
 
+# Contempt
+# Min Split Depth
+# Threads
+# Ponder
+# Hash
+# Multi PV
+# Skill Level
+# Move Overhead
+# Minimum Thinking Time
+# Slow Mover
+# UCI_Chess960
+# UCI_LIMIT_STRENGTH
+# UCI_Elo
+# depth
+# FEN
+
+
+# Methods
 @app.route('/', methods=['GET'])
 def home_page():
-    return ""
+    return "Welcome to the Home Page!. Read the documentation for more info!\nGithub: " \
+           "https://github.com/vigneshsaravanakumar404/Stockfish_API"
 
 
-@app.route('/user/', methods=['GET'])
-def request_page():
-    user_query = str(request.args.get('user'))
-    data_set = {'Output': user_query, 'Time': time.time()}
-    json_dump = json.dumps(data_set)
+@app.route('/engine/elo/', methods=['GET'])
+def new_engine_elo_page():
+    contempt = str(request.args.get('contempt'))
+    min_split_depth = str(request.args.get('min-split-depth'))
+    ponder = str(request.args.get('ponder'))
+    minimum_thinking_time = str(request.args.get('minimum-thinking-time'))
+    uci_chess960 = str(request.args.get('uci-chess960'))
+    uci_limit_strength = str(request.args.get('uci-limit-strength'))
+    fen = str(request.args.get('fen'))
+    if str(request.args.get('uci_elo')) == "None":
+        elo = 1350
+    else:
+        elo = int(request.args.get('uci_elo'))
 
-    print(user_query)  # Print user_query to the console
+    dict.append(['Contempt', contempt])
+    dict.append(['Min Split Depth', min_split_depth])
+    dict.append(['Ponder', ponder])
+    dict.append(['Minimum Thinking Time', minimum_thinking_time])
+    dict.append(['UCI_Chess960', uci_chess960])
+    dict.append(['UCI_LimitStrength', uci_limit_strength])
 
-    return json_dump
+    parameters = {}
+    for item in dict:
+        if item[1] != "None":
+            parameters[item[0]] = item[1]
+
+    # Set default value for fen if it is not provided
+    if fen is None:
+        fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+
+    stockfish.update_engine_parameters(parameters)
+    stockfish.set_fen_position(fen)
+    stockfish.set_elo_rating(int(elo))
+
+    return stockfish.get_best_move()
+
+
+@app.route('/engine/level/', methods=['GET'])
+def engine_level_page():
+    contempt = str(request.args.get('contempt'))
+    min_split_depth = str(request.args.get('min-split-depth'))
+    ponder = str(request.args.get('ponder'))
+    minimum_thinking_time = str(request.args.get('minimum-thinking-time'))
+    uci_chess960 = str(request.args.get('uci-chess960'))
+    uci_limit_strength = str(request.args.get('uci-limit-strength'))
+    fen = str(request.args.get('fen'))
+    if str(request.args.get('skill-level')) == "None":
+        skillLevel = 1350
+    else:
+        skillLevel = int(request.args.get('skill-level'))
+
+    dict.append(['Contempt', contempt])
+    dict.append(['Min Split Depth', min_split_depth])
+    dict.append(['Ponder', ponder])
+    dict.append(['Minimum Thinking Time', minimum_thinking_time])
+    dict.append(['UCI_Chess960', uci_chess960])
+    dict.append(['UCI_LimitStrength', uci_limit_strength])
+
+    parameters = {}
+    for item in dict:
+        if item[1] != "None":
+            parameters[item[0]] = item[1]
+
+    # Set default value for fen if it is not provided
+    if fen is None:
+        fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+
+    stockfish.update_engine_parameters(parameters)
+    stockfish.set_fen_position(fen)
+    stockfish.set_skill_level(int(skillLevel))
+
+    return stockfish.get_best_move()
+
+
+@app.route('/is-fen-valid/', methods=['GET'])
+def is_fen_valid_page():
+    fen = str(request.args.get('fen'))
+    return str(stockfish.is_fen_valid(fen))
+
+
+@app.route('/is-move-correct/', methods=['GET'])
+def is_move_correct_page():
+    fen = str(request.args.get('fen'))
+    move = str(request.args.get('move'))
+    stockfish.set_fen_position(fen)
+    return str(stockfish.is_move_correct(move))
+
+
+@app.route('/get-top-moves/', methods=['GET'])
+def get_top_moves_page():
+    fen = str(request.args.get('fen'))
+    number = int(request.args.get('number'))
+    stockfish.set_fen_position(fen)
+    return str(stockfish.get_top_moves(number))
+
+
+@app.route('/get-evaluation/', methods=['GET'])
+def get_evaluation_page():
+    fen = str(request.args.get('fen'))
+    stockfish.set_fen_position(fen)
+    return str(stockfish.get_evaluation())
 
 
 def main():
+    print(time.asctime(time.localtime(time.time())) + ": Starting server...")
     app.run(port=7777)
-
-    # Stock Fish cmds
-    # stockfish = Stockfish(path=r"C:\Users\Vigne\Downloads\stockfish_15.1_win_x64_popcnt\stockfish_15.1_win_x64_popcnt\stockfish-windows-2022-x86-64-modern.exe", depth=18, parameters={"Threads": 2, "Minimum Thinking Time": 30})
-    # stockfish.set_fen_position("r1bqkbnr/pppp1ppp/2n5/4p3/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 0 1")
-    # print(stockfish.get_best_move())
-
-    input("Press enter to close the window. >")
+    print(time.asctime(time.localtime(time.time())) + ": Server started!")
 
 
 # Run as admin
@@ -44,7 +152,7 @@ if __name__ == "__main__":
     else:
         main()
 
-    # How to start
+# How to start
 """Fire it up: ngrok http 7777"""
 
 # how to access from computer
@@ -59,13 +167,5 @@ headers = {
 response = requests.get(url, headers=headers)
 # Do something with the response
 print(response.text)
-
-"""
-
-# Json output code
-"""
-
-data_set = {'Page': 'Home', 'Message': 'Welcome to the home page!', 'Time': time.time()}
-    json_dump = json.dumps(data_set)
 
 """
